5c7f890f99dcfca93650f04069d40050b37be66d
Joakim Strandell <joakim@awkwardgroup.com>
2026-01-21 22:35:35 +0100

    Update Prettier and VSCode settings

--- packages/vue/src/components/Alert.vue
+++ packages/vue/src/components/Alert.vue
@@ -71,12 +71,7 @@ function onDismiss() {
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M5 13l4 4L19 7"
-        />
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
       </svg>
       <!-- Warning icon -->
       <svg
