a84e6c2c48bea03b1dc9ee748453fdd009f50de0
Joakim Strandell <joakim@awkwardgroup.com>
2026-01-27 11:40:09 +0100

    fix(eslint-config): allow linting mjs/cjs/js files

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -14,6 +14,7 @@ const baseConfig: object[] = [
       },
       parserOptions: {
         projectService: true,
+        allowDefaultProject: ['**/*.mjs', '**/*.cjs', '**/*.js'],
       },
     },
   },
